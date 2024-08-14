d18410097a72f88f26fbba79d1e77451b4f90b46
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 19:03:51 -0400

    Container Testing 1

--- front-end/src/components/customerHomePage/CustomerHomePage.tsx
+++ front-end/src/components/customerHomePage/CustomerHomePage.tsx
@@ -1,4 +1,4 @@
-import { Carousel } from "react-bootstrap";
+import { Carousel, Col, Container, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import { CardComponent } from "./CardComponent";
 import { HotelInterface } from "../../interfaces/HotelInterface";
@@ -27,20 +27,86 @@ const mockData: HotelInterface[] =[
     {
         hotelId:6,
         hotelName:"Hotel6"
+    },
+    {
+        hotelId:1,
+        hotelName:"Hotel21"
+    },
+    {
+        hotelId:2,
+        hotelName:"Hotel22"
+    },
+    {
+        hotelId:3,
+        hotelName:"Hotel23"
+    },
+    {
+        hotelId:4,
+        hotelName:"Hotel24"
+    },
+    {
+        hotelId:5,
+        hotelName:"Hotel25"
+    },
+    {
+        hotelId:6,
+        hotelName:"Hotel26"
+    },
+    {
+        hotelId:1,
+        hotelName:"Hotel31"
+    },
+    {
+        hotelId:2,
+        hotelName:"Hotel32"
+    },
+    {
+        hotelId:3,
+        hotelName:"Hotel33"
+    },
+    {
+        hotelId:4,
+        hotelName:"Hotel34"
+    },
+    {
+        hotelId:5,
+        hotelName:"Hotel35"
+    },
+    {
+        hotelId:6,
+        hotelName:"Hotel36"
+    },
+    {
+        hotelId:1,
+        hotelName:"Hotel41"
+    },
+    {
+        hotelId:2,
+        hotelName:"Hotel42"
+    },
+    {
+        hotelId:3,
+        hotelName:"Hotel43"
+    },
+    {
+        hotelId:4,
+        hotelName:"Hotel44"
+    },
+    {
+        hotelId:5,
+        hotelName:"Hotel45"
+    },
+    {
+        hotelId:6,
+        hotelName:"Hotel46"
     }
 ]
 
 export const CustomerHomePage: React.FC = () => {
 
-    const e = [];
+    
 
-    for (let i = 0; i < 6; i++) {
-        mockData.map((data, index) =>{
-            
-        })
-        e.push(<CardComponent  key={i} />);
-        
-    }
+    
 
   return (
     <div>
@@ -52,15 +118,18 @@ export const CustomerHomePage: React.FC = () => {
         <p>Search Bar Here</p>
         <p>
           Search Results Here
-          <div className="d-flex bg-danger ">
-            {e}
+          
+          <div className="d-flex bg-danger justify-content-center">
+            {mockData.map((hotel) =>(
+                <CardComponent key={hotel.hotelId} {...hotel}></CardComponent>
+            ))}
           </div>
         </p>
       </div>
-      <div>
+      <div className="bg-primary overflow-auto">
         <p>Trending Hotels Here
-        <Carousel fade={true} controls={true} interval={null} variant="dark">
-            <Carousel.Item className="d-flex col-5 justify-content-center">
+        <Carousel fade={true} controls={true} interval={null} variant="dark"className="bg-danger">
+            <Carousel.Item className="d-flex col-5 justify-content-center bg-secondary">
               <CardComponent></CardComponent>
               <CardComponent></CardComponent>
               <CardComponent></CardComponent>
