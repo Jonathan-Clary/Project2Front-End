e0b188bd32efc120e62b419eae48797a514daad3
Justin-swinney <Justinswinney1@icloud.com>
2024-08-15 14:53:00 -0400

    Updated UI and Filtering logic

--- front-end/src/components/admin/support-tickets/SupportTickets.tsx
+++ front-end/src/components/admin/support-tickets/SupportTickets.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { SupportTicketTable } from '../support-ticket-table/SupportTicketTable'
 import { SupportTicketInterface } from '../../../interfaces/support-ticket/SuppportTicketInterface';
-import { Card, Container, Nav, Pagination } from 'react-bootstrap';
+import { Button, ButtonGroup, ButtonToolbar, Card, CardBody, Container, Dropdown, DropdownButton, Form, InputGroup, Nav, Pagination } from 'react-bootstrap';
 import { TicketStatusEnum } from '../../../enums/support-tickets/TicketStatusEnum';
 import { TicketTypeEnum } from '../../../enums/support-tickets/TicketTypeEnum';
 import './SuppportTicket.css';
@@ -17,7 +17,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 5,
+        supportTicketId: 2,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.TECHNICAL_ISSUES,
@@ -25,7 +25,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 4,
+        supportTicketId: 3,
         // User HERE
         status: TicketStatusEnum.RESOLVED,
         type: TicketTypeEnum.GENERAL,
@@ -33,7 +33,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 3,
+        supportTicketId: 4,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.PRIVACY,
@@ -41,7 +41,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 2,
+        supportTicketId: 5,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.FEEDBACK,
@@ -49,7 +49,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 1,
+        supportTicketId: 6,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.TECHNICAL_ISSUES,
@@ -57,7 +57,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 5,
+        supportTicketId: 7,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.TECHNICAL_ISSUES,
@@ -65,7 +65,39 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 4,
+        supportTicketId: 8,
+        // User HERE
+        status: TicketStatusEnum.RESOLVED,
+        type: TicketTypeEnum.INFORMATION,
+        description: "INFORMATION PROBLEM DESCRIPTION",
+        createdAt: new Date('2023-08-01T10:00:00Z')
+    },
+    {
+        supportTicketId: 9,
+        // User HERE
+        status: TicketStatusEnum.PENDING,
+        type: TicketTypeEnum.PRIVACY,
+        description: "PRIVACY PROBLEM DESCRIPTION",
+        createdAt: new Date('2023-08-01T10:00:00Z')
+    },
+    {
+        supportTicketId: 10,
+        // User HERE
+        status: TicketStatusEnum.PENDING,
+        type: TicketTypeEnum.FEEDBACK,
+        description: "FEEDBACK PROBLEM DESCRIPTION",
+        createdAt: new Date('2023-08-01T10:00:00Z')
+    },
+    {
+        supportTicketId: 11,
+        // User HERE
+        status: TicketStatusEnum.PENDING,
+        type: TicketTypeEnum.TECHNICAL_ISSUES,
+        description: "GENERAL PROBLEM DESCRIPTION",
+        createdAt: new Date('2023-08-01T10:00:00Z')
+    },
+    {
+        supportTicketId: 12,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.INFORMATION,
@@ -73,39 +105,7 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 3,
-        // User HERE
-        status: TicketStatusEnum.PENDING,
-        type: TicketTypeEnum.PRIVACY,
-        description: "PRIVACY PROBLEM DESCRIPTION",
-        createdAt: new Date('2023-08-01T10:00:00Z')
-    },
-    {
-        supportTicketId: 2,
-        // User HERE
-        status: TicketStatusEnum.PENDING,
-        type: TicketTypeEnum.FEEDBACK,
-        description: "FEEDBACK PROBLEM DESCRIPTION",
-        createdAt: new Date('2023-08-01T10:00:00Z')
-    },
-    {
-        supportTicketId: 1,
-        // User HERE
-        status: TicketStatusEnum.PENDING,
-        type: TicketTypeEnum.TECHNICAL_ISSUES,
-        description: "GENERAL PROBLEM DESCRIPTION",
-        createdAt: new Date('2023-08-01T10:00:00Z')
-    },
-    {
-        supportTicketId: 5,
-        // User HERE
-        status: TicketStatusEnum.PENDING,
-        type: TicketTypeEnum.TECHNICAL_ISSUES,
-        description: "TECHNICAL_ISSUES PROBLEM DESCRIPTION",
-        createdAt: new Date('2023-08-01T10:00:00Z')
-    },
-    {
-        supportTicketId: 4,
+        supportTicketId: 13,
         // User HERE
         status: TicketStatusEnum.PENDING,
         type: TicketTypeEnum.GENERAL,
@@ -113,20 +113,20 @@ const mockData: SupportTicketInterface[] = [
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 3,
+        supportTicketId: 14,
         // User HERE
-        status: TicketStatusEnum.PENDING,
+        status: TicketStatusEnum.RESOLVED,
         type: TicketTypeEnum.PRIVACY,
         description: "PRIVACY PROBLEM DESCRIPTION",
         createdAt: new Date('2023-08-01T10:00:00Z')
     },
     {
-        supportTicketId: 2,
+        supportTicketId: 15,
         // User HERE
-        status: TicketStatusEnum.PENDING,
+        status: TicketStatusEnum.RESOLVED,
         type: TicketTypeEnum.FEEDBACK,
         description: "FEEDBACK PROBLEM DESCRIPTION",
-        createdAt: new Date('2023-08-01T10:00:00Z')
+        createdAt: new Date()
     },
 ];
 
@@ -136,47 +136,42 @@ const mockData: SupportTicketInterface[] = [
         2. FILTER BY TICKET TYPE (INFORMATION, PRIVACY, FEEDBACK, TECHNICAL_ISSUES, GENERAL )
         3. SET UP PAGINATION
         4. 
-
 */
 
 
 export const SupportTickets: React.FC = () => {
+
     const [filterData, setFilterData] = useState(mockData);
-    const [typeFilter, setTypeFilter] = useState('General');
+    const [typeFilter, setTypeFilter] = useState('Type');
+    const [typeFilterBtnTitle, setTypeFilterBtnTitle] = useState('Type');
+    const [statusTab, setStatusTab] = useState('Pending');
 
 
     const handleTabFilters = () => {
-        let filtered;
-        switch (typeFilter) {
-            case 'General':
-                filtered = mockData.filter(mockData => mockData.type == TicketTypeEnum.GENERAL && mockData.status == 'PENDING');
+        setTypeFilterBtnTitle(typeFilter);
+        let filtered = mockData;
 
-                break;
-            case 'Technical_Issues':
-                filtered = mockData.filter(mockData => mockData.type == TicketTypeEnum.TECHNICAL_ISSUES && mockData.status == 'PENDING');
-                break;
-            case 'Information':
-                filtered = mockData.filter(mockData => mockData.type == TicketTypeEnum.INFORMATION && mockData.status == 'PENDING');
-                break;
-            case 'Feedback':
-                filtered = mockData.filter(mockData => mockData.type == TicketTypeEnum.FEEDBACK && mockData.status == 'PENDING');
-                break;
-            case 'Privacy':
-                filtered = mockData.filter(mockData => mockData.type == TicketTypeEnum.PRIVACY && mockData.status == 'PENDING');
-                break;
-            default:
-                filtered = mockData;
+        if (typeFilter != 'Type') {
+            if (typeFilter.toUpperCase() in TicketTypeEnum) {
+                filtered = filtered.filter(data => 
+                    data.type === TicketTypeEnum[typeFilter.toUpperCase() as keyof typeof TicketTypeEnum]
+                );
+            }
+        }
+
+        if (statusTab) {
+            filtered = filtered.filter(data => data.status.toUpperCase() === statusTab.toUpperCase());
         }
         setFilterData(filtered);
     }
 
     useEffect(() => {
         handleTabFilters();
-    }, [typeFilter])
+    }, [typeFilter, statusTab])
 
     return (
         <Container>
-            <Card className='mt-5'>
+            {/* <Card className='mt-5'>
                 <Card.Header>
                     <Nav justify variant="tabs" defaultActiveKey="#General">
                         <Nav.Item onClick={() => setTypeFilter('General')}>
@@ -200,7 +195,7 @@ export const SupportTickets: React.FC = () => {
                     <Card.Title>Support Tickets</Card.Title>
                     <SupportTicketTable columnNames={["ID", "Type", "Status", "Description", "Creation Date"]} data={filterData} />
 
-                    {/* PAGINATION */}
+                     PAGINATION 
 
                     <Pagination className="justify-content-end">
                         <Pagination.Prev>Previous</Pagination.Prev>
@@ -208,10 +203,59 @@ export const SupportTickets: React.FC = () => {
                         <Pagination.Next>Next</Pagination.Next>
                     </Pagination>
                 </Card.Body>
+            </Card> */}
+
+            <Card className='mt-5 shadow'>
+                <Card.Header>
+                    <Nav justify variant="tabs" defaultActiveKey="#pending-tickets">
+                        <Nav.Item >
+                            <Nav.Link href="#pending-tickets" onClick={() => setStatusTab('Pending')}>Pending</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#resolved-tickets" onClick={() => setStatusTab('Resolved')}>Resolved</Nav.Link>
+                        </Nav.Item>
+
+                    </Nav>
+                </Card.Header>
+
+                <Card.Body>
+                    <Card.Title className='text-center m-4'>Support Tickets</Card.Title>
+                    <Card className='card text-center'>
+
+                        <Card.Header className=''>
+                            <ButtonToolbar aria-label="Toolbar with Button groups" className='d-flex justify-content-between'>
+                                <InputGroup>
+                                    <InputGroup.Text id="btnGroupAddon"><i className="bi bi-search"></i></InputGroup.Text>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="search.."
+                                    />
+                                </InputGroup>
+                                <ButtonGroup className="me-2" aria-label="First group">
+                                    <DropdownButton as={ButtonGroup} title={typeFilterBtnTitle} id="bg-nested-dropdown">
+                                        <Dropdown.Item onClick={() => setTypeFilter('General')}>General</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setTypeFilter('Technical Issues')}>Technical Issue</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setTypeFilter('Information')}>Information</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setTypeFilter('Feedback')}>Feedback</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setTypeFilter('Privacy')}>Privacy</Dropdown.Item>
+                                    </DropdownButton>
+                                    <Button variant='secondary'><i className="bi bi-arrow-clockwise"></i></Button>
+                                </ButtonGroup>
+                            </ButtonToolbar>
+                        </Card.Header>
+
+                        <Card.Body>
+                            <SupportTicketTable columnNames={["ID", "Type", "Status", "Description", "Creation Date"]} data={filterData} />
+                            {/* PAGINATION */}
+                            <Pagination className="justify-content-end">
+                                <Pagination.Prev>Previous</Pagination.Prev>
+
+                                <Pagination.Next>Next</Pagination.Next>
+                            </Pagination>
+                        </Card.Body>
+                    </Card>
+                </Card.Body>
             </Card>
-
-
         </Container>
-
-    )
+    );
 }
