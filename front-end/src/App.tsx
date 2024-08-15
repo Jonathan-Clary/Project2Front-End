5429f546e52797628658987b121a053d52dd3445
Mohammad Kabakibi <Mohammad.2001k@hotmail.com>
2024-08-15 00:32:11 -0400

    update profile info

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Temp } from './components/Temp';
-import UserProfile from './components/user/profile/UserProfile';
-import { Col, Container, Row } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
     <div className="App">
 
-     {/* <h1>TESTING BOOTSTRAP</h1>
-     <Temp></Temp> */}
-
-
-
-    <Container fluid className='mt-5'>
-        <Row className='d-flex justify-content-center align-items-start'>
-          <Col xs={10} style={{padding:0}} className='d-flex justify-content-center align-items-start'>
-            { /** Rendered page automatically goes here */  }
-            <div className='d-flex flex-column align-items-center justify-content-start' style={{margin: 0, padding: 0 }}>
-            <UserProfile />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-
+     <h1>TESTING BOOTSTRAP</h1>
+     <Temp></Temp>
     </div>
   );
 }
