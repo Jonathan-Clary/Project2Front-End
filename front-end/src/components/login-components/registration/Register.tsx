1cbf7cdf1547ecc26b3207d800fbe6f4ab597824
Katie-Cornette <katiecornette424@gmail.com>
2024-08-22 15:25:18 -0400

    register

---
+++ front-end/src/components/login-components/registration/Register.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export const Register: React.FC = () => {
+
+    const[errorMessage, setErrorMessage] = useState('')
+
+
+    return(
+        <div className="d-flex justify-content"></div>
+    )
+}
