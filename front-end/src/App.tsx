6619f40f29270ccd21c06773e7004cf6fbcba4c1
Justin-swinney <Justinswinney1@icloud.com>
2024-08-23 13:12:26 -0400

    minor changes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Temp } from './components/Temp';
-import UserProfile from './components/user/profile/UserProfile';
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
 
-     <h1>TESTING BOOTSTRAP</h1>
-     {/* <Temp></Temp> */}
-     <UserProfile />
+    
     </div>
   );
 }
