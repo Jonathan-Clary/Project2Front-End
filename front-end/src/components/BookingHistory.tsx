700cf50f95476e13468947019b01d2f3e5761200
Katie-Cornette <katiecornette424@gmail.com>
2024-08-25 18:32:10 -0400

    New changes to booking history

--- front-end/src/components/BookingHistory.tsx
+++ front-end/src/components/BookingHistory.tsx
@@ -10,19 +10,59 @@ export const BookingHistory: React.FC = () => {
     const{token, user} = useAuth()
     const axiosInstance = createAxiosInstance(token)
     const[bookingHistoryData, setHistoryData] = useState<BookingInterface[]>([])
+    const[upcomingBookingData, setUpcomingBooking] = useState<BookingInterface[]>([])
+    const[pastBookingData, setPastBooking] = useState<BookingInterface[]>([])
+    
 
-   
 
+    //Getting the current date. Have to add 1 to the month since janurary is 0 
+    const current = new Date()
+    //Testing the date works
+    //const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`
+
+    const filterBookingHistory = (allBookings: BookingInterface[], currentDate: Date) => {
+        const upcomingBookings: BookingInterface[] = []
+        const pastBookings: BookingInterface[] = []
+        allBookings.forEach((booking)=> {
+            console.log("booking", booking)
+            const bookingstartDate = new Date(booking.bookedDate);
+
+            //Getting rid of the time stamp
+            const bookingDateOnly = new Date(bookingstartDate.toDateString())
+            const currentDateOnly = new Date(currentDate.toDateString())
+
+            //Filtering
+            if(bookingDateOnly>currentDateOnly){
+                upcomingBookings.push(booking)
+            } else{
+                pastBookings.push(booking)
+            }
+        })
+        return {upcomingBookings, pastBookings}
+    }
+
+    //Method for getting all booking history and filtering if upcoming or past booking
     const getAllBookingHistory = async () => {
         try{
             const response = await axiosInstance.get(`/stays/user/${user?.userId}`)
-            console.log(response)
+            console.log("all bookings" , response)
+            setHistoryData(response.data)
+            console.log("booking history", response.data)
+            const {upcomingBookings, pastBookings} = filterBookingHistory(bookingHistoryData, current)
+            
+            setUpcomingBooking(upcomingBookings)
+            setPastBooking(pastBookings)
+
+            console.log("Upcoming Bookings: ", upcomingBookingData)
+            console.log("Past Booking: ", pastBookingData)
+          
         } catch(error){
             console.log("error occurred")
         }
     }
     useEffect(() => {
         getAllBookingHistory()
+        
     },[])
 
 
@@ -31,9 +71,10 @@ export const BookingHistory: React.FC = () => {
             <Container className="mt-5 mb-5 p-2" style={{background:"red"}}>
                 {/*Add functionality so if no upcoming bookings then display no upcoming bookings */}
                 <h3 className="mb-4">Upcoming Booking </h3>
-                
-                <BookingCard  className="w-25"></BookingCard>
-                
+                {upcomingBookingData.length === 0 ? <p>No Upcoming Trips</p> :
+                     <BookingCard  className="w-25"></BookingCard>
+                }
+        
             </Container>
             <Container className="mt-5 mb-5 p-2" style={{background:"red"}}>
                 <h3 className="mb-5">Past Booking</h3>
@@ -55,7 +96,9 @@ export const BookingHistory: React.FC = () => {
                     </InputGroup>
                     
                 </ButtonToolbar>
-                <BookingCard className="w-25"></BookingCard>
+                {pastBookingData.length === 0 ? <p> No Past Bookings</p> :
+                    <BookingCard className="w-25"></BookingCard>
+                }
             </Container>
             
         </div>
