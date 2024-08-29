8c3ecaafa68a2b0ee7718c20e67c5b7e6253ff57
Jonathan Clary <jonathanbclary@gmail.com>
2024-08-29 18:46:29 -0400

    Change to route to EC2

--- front-end/src/services/AxiosInstance.tsx
+++ front-end/src/services/AxiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = (token: string | null) => {
   const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://18.206.57.93:8080/',
     withCredentials: false,
     headers: {
       'Content-Type': 'application/json',
