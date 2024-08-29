45cfe528bc3e3635341427149ec77550d301cb87
BuildTools <unconfigured@null.spigotmc.org>
2024-08-29 14:55:14 -0400

    Local Branch

--- front-end/src/services/AxiosInstance.tsx
+++ front-end/src/services/AxiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = (token: string | null) => {
   const axiosInstance = axios.create({
-    baseURL: 'http://44.201.168.200:8080/',
+    baseURL: 'http://localhost:8080/',
     withCredentials: false,
     headers: {
       'Content-Type': 'application/json',
